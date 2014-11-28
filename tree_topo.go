ad5dd39921eb552e4af76bd3d508e43f713c9f59
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-27 22:26:32 -0800

    fix comments of NewTreeToplogy

--- tree_topo.go
+++ tree_topo.go
@@ -43,7 +43,7 @@ func (t *TreeTopology) SetNumberOfTasks(nt uint64) {
 }
 
 // Creates a new tree topology with given fanout and number of tasks.
-// This will be called each
+// This will be called in the task graph configuration phase.
 func NewTreeTopology(fanout, nTasks uint64) *TreeTopology {
 	m := &TreeTopology{
 		fanout:     fanout,
