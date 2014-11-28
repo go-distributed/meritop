5a1e114265a770e90d406afc49b23542c7764215
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2014-11-27 18:25:31 -0800

    regression test is running, but these is a epoch mismatch fatal

--- tree_topo.go
+++ tree_topo.go
@@ -44,10 +44,10 @@ func (t *TreeTopology) SetNumberOfTasks(nt uint64) {
 
 // Creates a new tree topology with given fanout and number of tasks.
 // This will be called each
-func NewTreeTopology(fanout, maxID uint64) *TreeTopology {
+func NewTreeTopology(fanout, nTasks uint64) *TreeTopology {
 	m := &TreeTopology{
 		fanout:     fanout,
-		numOfTasks: maxID + 1,
+		numOfTasks: nTasks,
 	}
 	return m
 }
